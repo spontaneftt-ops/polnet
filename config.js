8bd9e2f139e13939ab82812871506515da014384
spontaneftt-ops <spontaneftt@gmail.com>
2026-01-21 08:26:42 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// Firebase SDK'larını HTML içinde çağıracağız, burası sadece ayarlar.
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD4mXN8BZ3OMzl35vtEbNGA9BUuHPgoqQU",
+    authDomain: "mit-sistemi.firebaseapp.com",
+    databaseURL: "https://mit-sistemi-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mit-sistemi",
+    storageBucket: "mit-sistemi.firebasestorage.app",
+    messagingSenderId: "195453377116",
+    appId: "1:195453377116:web:85067f5070ecf983bb6499"
+};
+
+// Uygulamayı Başlat
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
